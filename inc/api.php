c682bc6a8a8e7234881b77f3d08de8cc643d0640
U-mousetrapActual\mouse <mouse@mousetrapActual.(none)>
2013-02-25 15:33:28 +0100

    some commenting and indentations

--- inc/api.php
+++ inc/api.php
@@ -1,29 +1,26 @@
 <?php
 
-// This is the API, 2 possibility show the user list, and show a specifique user by action.
+// This is the API...
 
 if($_SERVER['REQUEST_METHOD'] == "POST" || $_SERVER['REQUEST_METHOD'] ==  "PUT" || $_SERVER['REQUEST_METHOD'] ==  "DELETE" ){
   do_photo_stuff();
 }else{
-  get_user_list();
+  get_all_photos();
 }  
 
-function do_photo_stuff()
-{
-  
+function do_photo_stuff(){
+
   if($_SERVER['REQUEST_METHOD'] == "DELETE" ){
-  $headers = apache_request_headers();
-  foreach ($headers as $header => $value) {
-      
-      if ($header === 'id'){
+    $headers = apache_request_headers();
+    foreach ($headers as $header => $value) {      
+      if($header === 'id'){
         $modelDeleteID = $value;
       }else if($header === 'link'){
         $photo_link = '../'.$value;
       }
-  }
-
-}else{
-  
+    }//End of if server delete
+}else{//means it wasnt delete, probably POST OR PUT
+  //Decode json and set variables
   $box_data = json_decode(file_get_contents('php://input'));
   $jsonid = $box_data->{'id'};
   $a = $box_data->{'name'};
@@ -32,64 +29,50 @@ function do_photo_stuff()
   $d = $box_data->{'description'};
   $e = $box_data->{'public'};
 }
-  //$x = $_GET['name'];
-  //$y = $_GET['pass'];
-  
+ //open dbconnection 
 $mysqli = new mysqli("localhost", "root", "", "photobrawler");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
   }
-  // make a call in db.
 
 //Switch for post or put or smthn else
-
-  switch ($_SERVER['REQUEST_METHOD']) {
+  switch ($_SERVER['REQUEST_METHOD']){
     case "POST":
         $stmt = $mysqli->prepare("INSERT INTO photos (id, name, owner_id, link, description, public) VALUES (?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("isissi", $jsonid, $a, $b, $c, $d, $e); 
         $stmt->execute();
+        //Have to return the print
         print_r($box_data);
         break;
     case "PUT":
         $stmt = $mysqli->prepare("UPDATE photos SET name=?, owner_id=?, link=?, description=?, public=? WHERE id=?");
         $stmt->bind_param("sissii", $a, $b, $c, $d, $e, $jsonid); 
         $stmt->execute();
+        //Have to return the print
         print_r($box_data);
         break;
     case "DELETE":
         $stmt = $mysqli->prepare("DELETE FROM photos WHERE id=?");
         $stmt->bind_param("i", $modelDeleteID); 
         $stmt->execute();
-
+        //Delete the file from directory
         unlink($photo_link);
         break;
 }
- 
- 
-     
-    
-    
-    
+    //Id something went wrong
     if($stmt->error){
-    printf("Error: %s.\n", $stmt->error);
+      printf("Error: %s.\n", $stmt->error);
     }
-
-    
-    
-
   
 }
 
-
-
-function get_user_list()
-{
+function get_all_photos(){
   
   $mysqli = new mysqli("localhost", "root", "", "photobrawler");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
   }
-  // make a call in db.
+  
 
  $stmt = $mysqli->prepare(
       "SELECT id, name, owner_id, link, description, public FROM photos");
@@ -99,7 +82,6 @@ if ($mysqli->connect_errno) {
     $result = array();
     while($row1 = $stmt->fetch()) {
       $arr = "{'id':".$id.", 'email': ".$name."}";
-      //$arr = json_encode($arr, JSON_FORCE_OBJECT);
       $results['id'] = $id;
       $results['name'] = $name;
       $results['owner_id'] = $owner_id;
@@ -108,12 +90,7 @@ if ($mysqli->connect_errno) {
       $results['public'] = $public;
       array_push($result, $results);
           }
- 
-  //print_r($user_list);
   print_r(json_encode($result));
-  
-  
-    //$post_data = json_encode(array('item' => $post_data), JSON_FORCE_OBJECT);
   return json_encode($result);
 }
 
