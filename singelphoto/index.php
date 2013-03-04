2ef4f4ca84695e0a01a6d92e238b80957daa3bd6
clvenstrand <christoffer@rbd.nu>
2013-03-04 13:34:17 +0100

    added page for singelphoto

---
+++ singelphoto/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+	<?php
+		include_once ('../inc/head2.php');
+	?>
+</head>
+<body>
+	<div class="container">
+		<div class="topBanner">
+			PhotoBrawler
+
+            <?php if(isset($_SESSION['email'])){
+                echo 'Logged in';
+            } ?>
+
+	    </div>
+       
+		<div class="photoSingel">
+       		<div class="photoSingel">
+            
+            </div>
+            <div class="photoInfo">
+            
+            </div>
+       	</div>
+        
+	</div>
+<body>
+</html>
