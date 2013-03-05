5c0bfe30eb52013dffedae867223d4a9eb374e90
U-mousetrapActual\mouse <mouse@mousetrapActual.(none)>
2013-03-05 15:24:49 +0100

    login marked up as mobile

--- login/index.php
+++ login/index.php
@@ -1,50 +1,44 @@
 <!DOCTYPE html>
 <head>
 	<?php
-		include_once ('../inc/head2.php');
-	?>
+  include_once ('../inc/head2.php');
+  ?>
 </head>
 <body>
-	<div class="container">
-		<div class="topBanner">
+	<div class="container" id="yesss" data-role="page">
+		<div class="topBanner" data-role="header">
 			PhotoBrawler
 
             <?php if(isset($_SESSION['email'])){
                 echo 'Logged in';
             } ?>
 
-	    </div>
+        </div><!-- /end header -->
 
-        <div id="login-content" class="loginCont">
-            <!--<form action="../inc/login/login.php" method="post" >
-                <fieldset id="inputs">
-                    <input id="email" type="email" name="email" placeholder="Your Email" required>
-                    <input id="password" type="password" name="password" placeholder="Password" required>
-                </fieldset>
-                <fieldset id="actions">
-                    <input type="submit" id="submit" value="Log in">
-                </fieldset>
-            </form>-->
-            
-            <form action="../inc/login/login.php" method="post">
+        <div id="login-content" class="loginCont" data-role="content">
+                       
+            <form action="../inc/login/login.php" method="post" data-transition="slide">
             	<fieldset>
-                
+
                     <label>Email</label>
                     <div class="input-prepend">
                     	<span class="add-on"><i class="icon-user"></i></span>
-                		<input class="span3 control-label" type="text" id="email" placeholder="Email" name="email" required>
-                    </div>
-                    
-                    <label>Password</label>
-                    <div class="input-prepend">
-                    	<span class="add-on"><i class="icon-lock"></i></span>
-						<input class="span3 control-label" type="password" id="password" name="password" placeholder="Password" required>
-					</div><br />
-                  	<button type="submit" class="btn" id="submit">Sign in</button>
-                	
-            	</fieldset>
-            </form>
-        </div>
-	</div>
+                      <input class="span3 control-label" type="text" id="email" placeholder="Email" name="email" required>
+                  </div>
+
+                  <label>Password</label>
+                  <div class="input-prepend">
+                   <span class="add-on"><i class="icon-lock"></i></span>
+                   <input class="span3 control-label" type="password" id="password" name="password" placeholder="Password" required>
+               </div><br />
+               <button type="submit" class="btn" id="submit">Sign in</button>
+
+           </fieldset>
+       </form>
+   </div>
+   <div data-role="footer"> </div>
+   <script src="js/libs/jquery.mobile-1.3.0.min.js"></script>
+</div>
+
 <body>
-</html>
+    </html>
