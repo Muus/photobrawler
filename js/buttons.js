ded4f2a61d81d6a9649ee2dabea045fbdc39e026
clvenstrand <christoffer@rbd.nu>
2013-03-07 16:43:30 +0100

    good stuff

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,9 @@
 $(function () { 
    
+   
+   
+   
+   
     $('#mobileCamera').click(function (e) {
         e.preventDefault();
         $('#fileSelect').trigger('click');
@@ -7,8 +11,40 @@ $(function () {
  
     $('#fileSelect').change(function () {
        // var photoName = prompt('Name your photo');
+	   
         $('#photoName').val('kurre');
-        $("#mobileCameraForm")[0].submit();
+        //$("#mobileCameraForm")[0].submit(function(event) {
+		//return false;
+  /* stop form from submitting normally */
+  alert('yo');
+  event.preventDefault();
+
+  /*clear result div*/
+   //esult").html('');
+
+  /* get some values from elements on the page: */
+   var values = $(this).serialize();
+	console.log(values);
+  /* Send the data using post and put the results in a div */
+    $.ajax({
+      url: "/photobrawler/inc/upload_photo.php",
+      type: "post",
+      data: values,
+      success: function(){
+          alert(values);
+           $("#result").html('submitted successfully');
+      },
+      error:function(){
+          alert("failure");
+          $("#result").html('there is error while submit');
+      }   
     });
+	
+	 
+});
+    //});
+	
+	/* attach a submit handler to the form */
+
 
 });
