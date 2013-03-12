6e42f07f45ce7e2dad1251a7f8c7e705f2fff767
clvenstrand <christoffer@rbd.nu>
2013-03-12 12:07:31 +0100

    singelphoto update

--- singlephoto/index.php
+++ singlephoto/index.php
@@ -10,8 +10,6 @@ echo "<script>
 </script>"
 
 ?>
-<<<<<<< HEAD
-
 <div id="singelphoto" data-role="page" class="container" >
 	<div data-role="header" class="topBanner">
 		<div class="userBtn">
@@ -30,40 +28,10 @@ echo "<script>
 			<div class="photoSingel thumbnail">
 				<img id="dude" src="<?php echo '../'.$dat_link;?>" alt=" " />
 
-				<p>Name:</p><h3 class="photoTitle"></h3>
-				<p>Description:</p><article class="photoDescription" id="photoDesc""></article>
+				<h3 class="photoTitle"></h3>
+				<article class="photoDescription" id="photoDesc""></article>
 			</div>
 		</div>
 
 	</div><!-- /content -->
 </div>
-=======
-</head>
-<body>
-
-	<div id="singelphoto" data-role="page" class="container" >
-		<div data-role="header" class="topBanner">
-			<div class="userBtn"><a href="#" data-rel="back"><button class="btn"><i class="icon-circle-arrow-left"></i></button></a></div>
-		</div><!-- /header -->
-		<div data-role="content">
-
-			<div class="photoFrame">
-				<div class="photoSingel thumbnail">
-					<img id="dude" src="<?php echo '../'.$dat_link;?>" alt=" " />
-
-                    <p>Name:</p><h3 id="photTitle" class="photoTitle" style="border-style:solid;"></h3>
-					<p>Description:</p><article class="photoDescription" id="photoDesc" style="border-style:solid;"></article>
-				</div>
-			</div>
-
-		</div><!-- /content -->
-		<div data-role="footer" style="display:none;">
-			<h4>Page Footer</h4>
-		</div><!-- /header -->
-	</div>
-	<script>
-	
-	</script>
-	<body>
-		</html>
->>>>>>> 81157dfa56f94a2f52519ccdb5abfc71cc66aee9
